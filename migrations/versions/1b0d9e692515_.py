c1b14d29104ad8b83e933675afa41fca900e8cf0
teddyedwar29 <teddyedwar@gmail.com>
2025-08-04 10:53:13 +0700

    test perbaikan branch teddy

--- migrations/versions/1b0d9e692515_.py
+++ migrations/versions/1b0d9e692515_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: a89454733d2b
+Revision ID: 1b0d9e692515
 Revises: 
-Create Date: 2025-08-02 10:23:39.962287
+Create Date: 2025-08-04 10:43:59.183600
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'a89454733d2b'
+revision = '1b0d9e692515'
 down_revision = None
 branch_labels = None
 depends_on = None
